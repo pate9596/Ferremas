646d1c36d84a0d02d2c0d7bf4f49681a90a38cd6
pate9596 <deelanmaldonado9@gmail.com>
2024-05-02 00:57:41 -0400

    Modificaciones en views

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import home,catag,login,contador,bodeguero,vendedor,admin
+from . import views
 
 urlpatterns = [
-     path('', home, name="archivo"),
-     path('catalogo/', catag, name="catalogo"),
-     path('account/',login, name="login"),
-     path('contador/',contador,name="contador"),
-     path('Bodeguero/',bodeguero, name="bodeguero"),
-     path('vendedor/',vendedor,name="vendedor"),
-     path('administrador/',admin,name="admin"),
+     path('', views.home, name="archivo"),
+     path('catalogo/', views.catag, name="catalogo"),
+     path('account/',views.login, name="login"),
+     path('contador/',views.contador,name="contador"),
+     path('Bodeguero/',views.bodeguero, name="bodeguero"),
+     path('vendedor/',views.vendedor,name="vendedor"),
+     path('administrador/',views.admin,name="admin"),
 ]
